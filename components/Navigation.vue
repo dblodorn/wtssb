d5e8687bcb873d4f81512f2102c45247586965fb
Dain Blodorn Kim <dainblodornkim@dains-imac.lan>
2021-03-22 13:50:23 -0700

    navigation roughed in

--- components/Navigation.vue
+++ components/Navigation.vue
@@ -1,12 +1,12 @@
 <template>
-  <menu id="nav-container" class="flex-row pad-single">
+  <menu id="nav-container">
     <button 
-      v-for="item in slides" 
-      :key="`slide-button-${item}`"
-      class="lozenge-button small"
-      @click="cameraHandler(item)"
+      v-for="(item, index) in nav" 
+      :key="`slide-button-${index}`"
+      class="nav-button"
+      @click="cameraHandler(index + 1)"
     >
-      <span>Ball {{item}}</span>
+      <span class="bezier-300">{{item.title}}</span>
     </button>
   </menu>
 </template>
@@ -22,19 +22,107 @@ export default {
   },
   data() {
     return {
-      slides: [1,2,3,4,5,6]
+      slides: [1,2,3,4,5,6],
+      nav: [
+        {
+          title: 'Touch'
+        },{
+          title: 'Synesthesia'
+        },{
+          title: 'Sight'
+        },{
+          title: 'Hearing'
+        },{
+          title: 'Smell'
+        },{
+          title: 'Taste'
+        }
+      ]
     }
   }
 }
 </script>
 
 <style lang="css">
+  :root {
+    --nav-wrapper: 12rem;
+    --nav-dot: 2.5rem;
+    --nav-border: 2px solid white;
+    --nav-left: 6rem;
+  }
   #nav-container {
-    position: absolute;
-    bottom: 0;
-    left: 0;
+    position: fixed;
+    top: 6rem;
+    left: var(--nav-left);
     z-index: 12000;
+    width: var(--nav-wrapper);
+    height: var(--nav-wrapper);
+  }
+  #nav-container:after {
+    content: "";
+    position: absolute;
+    top: 0;
+    left: 0;
     width: 100%;
-    justify-content: space-between;
+    height: 100%;
+    border-radius: calc(var(--nav-wrapper) / 2);
+    border: 1px solid blue;
+    z-index: 0;
+  }
+  .nav-button {
+    width: var(--nav-dot);
+    height: var(--nav-dot);
+    border: var(--nav-border);
+    border-radius: calc(var(--nav-dot) / 2);
+    position: absolute;
+    z-index: 1;
+  }
+  .nav-button span {
+    position: fixed;
+    opacity: 0;
+    width: var(--nav-wrapper);
+    text-align: center;
+    top:  2rem;
+    left: var(--nav-left);
+    color: white;
+    font-family: var(--font-a);
+    font-size: 1.5rem;
+    padding-left: .5rem;
+  }
+  @media (hover:hover) {
+    .nav-button:hover {
+      background: var(--white);
+    }
+    .nav-button:hover span {
+      opacity: 1;
+    }
+  }
+  .nav-button:nth-of-type(1) {
+    top: calc((var(--nav-dot) / 2.25) * -1);
+    left: .5rem;
+    right: 0;
+    margin: auto;
+  }
+  .nav-button:nth-of-type(2) {
+    top: calc(var(--nav-wrapper) * .2);
+    right: calc((var(--nav-dot) / 4) * -1);
+  }
+  .nav-button:nth-of-type(3) {
+    top: calc(var(--nav-wrapper) * .7);
+    right: calc((var(--nav-dot) / 6) * -1);
+  }
+  .nav-button:nth-of-type(4) {
+    top: calc(var(--nav-wrapper) - (var(--nav-dot) / 2));
+    left: 0;
+    right: 0;
+    margin: auto;
+  }
+  .nav-button:nth-of-type(5) {
+    top: calc(var(--nav-wrapper) * .7);
+    left: 0;
+  }
+  .nav-button:nth-of-type(6) {
+    top: calc(var(--nav-wrapper) * .2);
+    left: calc((var(--nav-dot) / 6) * -1);
   }
 </style>
